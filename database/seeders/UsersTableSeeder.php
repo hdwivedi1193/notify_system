4ea55f54ef6d9a75577ed680bba065b652cd154f
Himanshu <dwivedi.himanshu1193@gmail.com>
2024-08-14 20:43:28 +0530

    Update User Table and Add Carbon Package
    - Modified user migration to add 'user_type' column with default value 'individual'
    - Updated existing migration for user table to reflect changes
    - Installed Carbon package for enhanced date and time manipulation
    -Removing migration for update_user_table_for_notifications

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Hash;
+use DB;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create 10 individual users
+        $faker = \Faker\Factory::create();
+        $now = Carbon::now();
+
+        DB::table("users")->insert([
+            "name" => $faker->name,
+            "email" => $faker->unique()->safeEmail,
+            "password" => Hash::make("password"),
+            'user_type' => 'individual',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        // Create 1 admin user
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'user_type' => 'admin',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+    }
+}
