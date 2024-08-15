343f4ddd489ab5ea082bfdfde7117be4f50d3a6b
Himanshu <dwivedi.himanshu1193@gmail.com>
2024-08-15 14:32:18 +0530

    Added migrations for user_notifications tables

---
+++ database/migrations/2024_08_14_205005_create_notification_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->onDelete('cascade');
+            $table->foreignId('notification_id')->onDelete('cascade');
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification_user');
+    }
+}
